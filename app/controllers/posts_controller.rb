be09cd0472341993f2547c408f09acd9ff45465a
naveen krishnan <naveenkrishnan01@gmail.com>
2013-11-24 08:46:51 -0800

    add rails 2 week 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
- before_action :set_post, only: [:show, :edit, :update]
-
+ before_action :set_post, only: [:show, :edit, :update, :vote]
+ before_action :require_user, except: [:index, :show, :vote]
+ 
   def index
 
-  @post = Post.all
+  @post = Post.all.sort_by{|x| x.total_votes}.reverse
   end
 
    def show
@@ -38,6 +39,17 @@ class PostsController < ApplicationController
        render :edit
      end
    end
+
+  def vote
+  @vote = Vote.create(voteable: @post, user_id: 1, vote: params[:vote])
+
+   if @vote.valid?
+     flash[:notice] = "Your vote was counted"
+   else
+     flash[:error] = "Your vote was not counted" 
+   end
+      redirect_to :back
+ end
 
     private
 
