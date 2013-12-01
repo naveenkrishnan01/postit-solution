3728bbdff6076ffd0decc9e93e4699fe34862525
naveen krishnan <naveenkrishnan01@gmail.com>
2013-12-01 00:16:04 -0800

    add ome more push

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129064247) do
+ActiveRecord::Schema.define(version: 20131117215332) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20131129064247) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
   end
 
   create_table "users", force: true do |t|
@@ -49,8 +48,6 @@ ActiveRecord::Schema.define(version: 20131129064247) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "role"
-    t.string   "time_zone"
   end
 
   create_table "votes", force: true do |t|
